1306264b702d4ec716a7aa9d9ece5861579692bd
Peter Ananwude <galaxypips67@gmail.com>
2026-09-20 13:35:28 +0100

    completed function basics and scoping

--- analyze.py
+++ analyze.py
@@ -1,10 +1,26 @@
-print("\nWelcome to the text analysis tool, i will mine and analyze a body of text from a file you give me")
+#Welcome
+def welcomeuser():
+ print("\nWelcome to the text analysis tool, i will mine and analyze a body of text from a file you give me")
 
 # Get a username
+def getusername():
+ #print message  prompting  user  get to get username into the terminal.
+ Usernamefrominput= input("\nTo begin please enter your username\n")
+ return Usernamefrominput
 
-#
-#Get input from user into the terminal.
-Username= input("\nTo begin please enter your username\n")
 
-#Greet The User.
-print("\nHello," + Username)
+#Greet user
+def greetuser (name, instruction):
+  print("\nHello," + name + "," + instruction)
+
+
+
+
+welcomeuser()
+username = getusername()
+greetuser(username, "get ready")
+
+
+
+
+
